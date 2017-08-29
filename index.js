8f699d9176fcc0e3acb34c150cbe97f4664534d3
leesc22 <leesiewching22@gmail.com>
2017-08-29 11:08:12 +0800

    Display restaurant when click 'Start'

--- index.js
+++ index.js
@@ -4,11 +4,31 @@ function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
 }
 
+function displayRestaurant() {
+	// create restaurant list
+	restaurantArray = ['Meet Mee', 'Surawon', 'Sushi Zanmai', 'Klang Teluk Pulai Bat Kut Teh', 'Dim Sum']
+	arrayLength = restaurantArray.length;
+	rand = getRandomInt(0, arrayLength);
+
+  // display restaurant
+  restaurant = document.getElementById('restaurant')
+  restaurant.innerHTML = restaurantArray[rand]	
+}
+
+function startDisplayRestaurant() {
+	// clear sentence
+	chosenRestaurant = document.getElementById('chosen-restaurant');
+	chosenRestaurant.innerHTML = "";
+	
+  // display list of restaurant in interval
+  var startTimer = setInterval(displayRestaurant, 2000);
+}
+
 function addRestaurantListener(){
   startBtn = document.getElementById('start-random-button');
   
   // Replace 'functionName' with the name of the function that you have written
-  startBtn.addEventListener("click", functionName)
+  startBtn.addEventListener("click", startDisplayRestaurant)
 
   getBtn = document.getElementById('get-restaurant-button');
   
